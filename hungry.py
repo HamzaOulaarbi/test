856863d96c3900cd1c8731857f553beba0e5cf61
OULAARBI Hamza <L057021@eu.labinal.snecma>
2022-06-08 13:46:31 +0200

    delete stop

--- hungry.py
+++ hungry.py
@@ -1,13 +1,12 @@
 print('hi')
 h=input("are you hungry ?")
 if h=="yes":
-	print('you can drink water')
-	print('eat pizza')
-	print('eat burger')
-	print('drink water')
-	print('drink juice')
-	print('eat fries')
-	print('stop eating')
+    print('you can drink water')
+    print('eat pizza')
+    print('eat burger')
+    print('drink water')
+    print('drink juice')
+    print('eat fries')
 #else :
 #	t=input('are you thirsty')
 #	if t=='yes':
