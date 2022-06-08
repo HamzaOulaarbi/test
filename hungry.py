0b50997504fd4c3a1fc4c8c067b18c3adcd389d2
OULAARBI Hamza <L057021@eu.labinal.snecma>
2022-06-08 14:27:25 +0200

    simple mainbranch

--- hungry.py
+++ hungry.py
@@ -1,9 +1,5 @@
 print('hi')
 h=input("are you hungry ?")
 if h=="yes":
-    print('you can drink water')
-    print('eat pizza')
-    print('eat burger')
     print('drink water')
-    print('drink juice')
-    print('eat fries')
+    print('eat pizza')
