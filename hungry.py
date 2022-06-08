a231546361a8618abc1f12512e4a8b540cb5417c
OULAARBI Hamza <L057021@eu.labinal.snecma>
2022-06-08 10:45:20 +0200

    add all

--- hungry.py
+++ hungry.py
@@ -1,1 +1,12 @@
 print('hi')
+h=input("are you hungry ?")
+if h=="yes":
+	print('eat pizza')
+	print('eat burger')
+	print('drink water')
+	print('drink juice')
+	print('eat fries')
+#else :
+#	t=input('are you thirsty')
+#	if t=='yes':
+#		print('drink water')
