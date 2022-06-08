b161bc257e99d8ebb17691bcb711d33df7ed9ced
OULAARBI Hamza <L057021@eu.labinal.snecma>
2022-06-08 13:52:13 +0200

    simple add

--- hungry.py
+++ hungry.py
@@ -1,13 +1,9 @@
 print('hi')
 h=input("are you hungry ?")
 if h=="yes":
-	print('eat pizza')
-	print('eat burger')
-	print('drink water')
-	print('drink juice')
-	print('eat fries')
-	print('stop eating')
-#else :
-#	t=input('are you thirsty')
-#	if t=='yes':
-#		print('drink water')
+    print('you can drink water')
+    print('eat pizza')
+    print('eat burger')
+    print('drink water')
+    print('drink juice')
+    print('eat fries')
