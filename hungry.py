f533a95bb85aa421ee1072b046562c41283ba416
OULAARBI Hamza <L057021@eu.labinal.snecma>
2022-06-08 11:43:33 +0200

    drink

--- hungry.py
+++ hungry.py
@@ -1,10 +1,3 @@
 h=input("are you hungry ?")
 if h=="yes":
-	print('eat pizza')
-	print('eat burger')
 	print('drink water')
-	print('drink juice')
-#else :
-#	t=input('are you thirsty')
-#	if t=='yes':
-#		print('drink water')
